97b6858bc8d14c35cf2ad166193f8186196ae76f
Lexus Drumgold <lex@flexdevelopment.llc>
2020-04-30 17:43:00 -0400

    🐛 FIX: SyntaxError: Cannot use import statement outside a module

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+/**
+ * @file Project Wide Babel Configuration
+ * @see {@link https://babeljs.io/docs/en/config-files}
+ */
+
+module.exports = api => {
+  api.cache(true)
+
+  return {
+    ignore: [
+      './node_modules/*',
+      './public/*',
+      './src/sass/*'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-throw-expressions'
+    ],
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ]
+  }
+}
