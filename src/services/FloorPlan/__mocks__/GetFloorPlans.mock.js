cc2f2e4d7f662b9ce5afeb3b405ffb78489abd7d
Lexus Drumgold <lex@flexdevelopment.llc>
2020-05-04 22:25:56 -0400

    📦 NEW: Mock Data - FloorPlan Service GET response

---
+++ src/services/FloorPlan/__mocks__/GetFloorPlans.mock.js
@@ -0,0 +1,25 @@
+/**
+ * @file Mock Data - FloorPlan Service GET response
+ * @module services/FloorPlans/__mocks__/GetFloorPlans
+ */
+
+export const GetFloorPlansMock = [{
+  PropertyId: '20006',
+  FloorPlanId: '431183',
+  FloorPlanName: 'Birch',
+  Beds: '1',
+  Baths: '1.0',
+  MinimumSqFt: '523',
+  MaximumSqFt: '523',
+  MinimumRent: '1359',
+  MaximumRent: '1513',
+  MinimumDeposit: '500',
+  MaximumDeposit: '500',
+  AvailableUnitsCount: '0',
+  AvailabilityURL: 'https://gardenviewterrace.securecafe.com/onlineleasing/gardenview-terrace/oleapplication.aspx?stepname=Apartments&myOlePropertyId=20006&floorPlans=969074',
+  FloorPlanImageURL: 'http://cdn.rentcafe.com/dmslivecafe/3/20006/3_20006_981101.jpg',
+  FloorPlanImageName: 'Gardenview1bedroom.jpg',
+  PropertyShowsSpecials: '0',
+  FloorPlanHasSpecials: '0',
+  UnitTypeMapping: 'ca011b1b'
+}]
